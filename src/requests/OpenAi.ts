bce1e5e409effc9ed462f696ad043a0ac937b13d
Nick Saulnier <nicholas.e.saulnier@gmail.com>
2024-07-23 09:35:22 -0700

    Add fetcher for OpenAI API chat completions

---
+++ src/requests/OpenAi.ts
@@ -0,0 +1,43 @@
+import OpenAI from 'openai';
+import {
+  ChatCompletionCreateParamsBase,
+  ChatCompletionMessageParam,
+} from 'openai/resources/chat/completions';
+import { Agent } from '../context/types';
+
+type OpenAIClientProps = {
+  baseURL: string;
+  apiKey: string;
+};
+
+type fetchOpenAICompletionProps = {
+  prompt: string;
+} & ChatCompletionCreateParamsBase &
+  OpenAIClientProps;
+
+export async function fetchOpenAICompletion({
+  prompt,
+  baseURL,
+  apiKey,
+  model,
+  messages,
+  ...props
+}: fetchOpenAICompletionProps) {
+  const client = new OpenAI({
+    baseURL: baseURL,
+    apiKey: apiKey,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const conversation: Array<ChatCompletionMessageParam> = [
+    ...messages,
+    { role: Agent.User, content: prompt },
+  ];
+  const completion = await client.chat.completions.create({
+    model,
+    messages: conversation,
+    ...props,
+  });
+
+  return completion;
+}
