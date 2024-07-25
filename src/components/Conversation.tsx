d2647269e286af70d62d3f68f68e9a4c0ad1f460
Nick Saulnier <nicholas.e.saulnier@gmail.com>
2024-07-25 09:21:31 -0700

    Style Conversation container

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -1,8 +1,8 @@
-import { ListItem, ListItemAvatar, ListItemText } from '@mui/material';
+import { Box, ListItem, ListItemAvatar, ListItemText, useTheme } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
 
 import type { Conversation } from '../context/types';
-import { formatTimestamp, truncateString } from './utils';
+import { formatTimestamp } from './utils';
 
 type ConversationProps = {
   conversation: Conversation;
@@ -10,20 +10,39 @@ type ConversationProps = {
 };
 
 export function Conversation({ conversation, index }: ConversationProps) {
+  const theme = useTheme();
+
   return (
     <ListItem key={`conversation-${index}`}>
-      <ListItemAvatar>
-        <ChatIcon />
-      </ListItemAvatar>
-      <ListItemText
-        primaryTypographyProps={{ color: 'text.secondary', fontSize: '15px', fontWeight: 700 }}
-        primary={
-          conversation.messages[0].content
-            ? truncateString(conversation.messages[0].content)
-            : 'Untitled'
-        }
-        secondary={formatTimestamp(conversation.messages[0].timestamp) ?? ''}
-      />
+      <Box sx={{ display: 'flex', alignItems: 'center', width: '75%' }}>
+        <ListItemAvatar sx={{ minWidth: '37px', maxWidth: '37px' }}>
+          <ChatIcon />
+        </ListItemAvatar>
+        <ListItemText
+          sx={{
+            // Directly style the span on mobile so that a breakpoint can be
+            // used for smaller fontSize styling
+            '& span': {
+              fontSize: '15px',
+              [theme.breakpoints.down('sm')]: {
+                fontSize: '13px',
+              },
+            },
+          }}
+          primaryTypographyProps={{
+            color: 'text.secondary',
+            fontWeight: 700,
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+          primary={conversation.messages[0].content ?? 'Untitled'}
+          secondary={formatTimestamp(conversation.messages[0].timestamp) ?? ''}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+        hi
+      </Box>
     </ListItem>
   );
 }
--- src/components/utils.ts
+++ src/components/utils.ts
@@ -17,10 +17,3 @@ export function formatTimestamp(timestamp: number) {
   const date = new Date(timestamp);
   return `${monthIndexToMonth[date.getMonth()]} ${date.getDay()}, ${date.getFullYear()}`;
 }
-
-export function truncateString(str: string, numChars = 30) {
-  if (str.length <= numChars) {
-    return str;
-  }
-  return str.slice(0, numChars) + '...';
-}
