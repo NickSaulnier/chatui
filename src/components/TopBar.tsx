fa8f840bb13d345436d38a8a75db658381c4a4aa
Nick Saulnier <nicholas.e.saulnier@gmail.com>
2024-08-29 10:03:18 -0700

    Add login authentication buttons to TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,15 @@
-import { Box } from '@mui/material';
+import { Box, Button, IconButton } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { AppMenu } from './AppMenu';
+import { AuthenticationContext } from '../context/AuthenticationContextProvider';
 
 export function TopBar() {
+  const { currentUser } = useContext(AuthenticationContext);
+  const navigate = useNavigate();
+
   return (
     <Box
       className="box"
@@ -17,6 +25,31 @@ export function TopBar() {
       }}
     >
       <AppMenu />
+      <Box
+        sx={(theme) => ({
+          marginRight: theme.spacing(2),
+          display: 'flex',
+          justifyContent: 'flex-end',
+          width: '100%',
+        })}
+      >
+        {currentUser ? (
+          <IconButton>
+            <AccountCircleIcon
+              fontSize="large"
+              sx={(theme) => ({ color: theme.palette.text.primary })}
+            />
+          </IconButton>
+        ) : (
+          <Button
+            variant="text"
+            sx={(theme) => ({ color: theme.palette.text.primary })}
+            onClick={() => navigate('/authenticate')}
+          >
+            Login
+          </Button>
+        )}
+      </Box>
     </Box>
   );
 }
